0d4b38ebb9b77ed86267f289e4942cbc938c135b
Vishal <pandevishal40@gmail.com>
2023-01-25 12:35:26 +0530

    - created stop watch project

---
+++ Stop_watch/javascript.js
@@ -0,0 +1,69 @@
+window.onload = function() {
+var seconds=00;
+var tens=00;
+var appendTens=document.getElementById("tens")
+var appendSeconds=document.getElementById("seconds")
+
+
+var buttonStart=document.getElementById("button-start");
+var buttonStop=document.getElementById("button-stop");
+var buttonReset=document.getElementById("button-reset")
+ var interval;
+
+ buttonStart.onclick=function() {
+
+clearInterval(interval);
+     interval=setInterval(startTimer,10);
+
+ }
+
+ buttonStop.onclick=function() {
+    clearInterval(interval);
+ }
+buttonReset.onclick=function() {
+clearInterval(interval);
+tens=00;
+seconds=00;
+appendTens.innerHTML=tens;
+
+appendSeconds.innerHtml=seconds;
+
+}
+
+function startTimer(){
+
+
+    tens++;
+
+    if(tens<=9){
+appendTens.innerHTML="0" + tens;
+
+    }
+    if(tens>9){
+appendTens.innerHTML=tens;
+    
+    }
+
+    if(tens>99){
+
+console.log("seconds");
+
+seconds++;
+appendSeconds.innerHtml="0"+seconds;
+tens=0;
+appendTens.innerHTML="0" + 0;
+
+    }
+
+if(seconds>9){
+
+    appendSeconds.innerHTML=seconds;
+}
+
+    }
+}
+
+
+
+
+
